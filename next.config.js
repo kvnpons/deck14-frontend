81921a8d6b1f77a2e734a40451d226975c499589
kevin.ponce <kevin.ponce@blotocol.com>
2025-07-17 12:06:42 +0800

    fix assets

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/deck14-frontend', // 👈 required for GitHub Pages
-    trailingSlash: true, // 👈 required for static hosting
-    assetPrefix: '/deck14-frontend/'
-  };
-  
-  module.exports = nextConfig;
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  output: 'export',
+  basePath: isProd ? '/kvnpons' : '',
+  assetPrefix: isProd ? '/kvnpons/' : '',
+};
